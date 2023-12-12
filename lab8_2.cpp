45d0ace4a97f473664b06b70b113028d6cba40c0
Your Name <you@example.com>
2023-12-12 14:40:12 +0700

    lab8_2.cpp completed

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -4,6 +4,19 @@
 using namespace std;
 
 //[Missing Code 1] Write definition of the function findGrade() here.
+char findGrade(float x){
+	if(x > 90){
+			return 'A';
+	}else if(x > 75 && x <= 90){
+			return 'B';
+	}else if(x > 60 && x <= 75){
+			return 'C';
+	}else if(x > 45 && x <= 60){
+			return 'D';
+	}else
+		return 'F';
+	}
+
 
 int main(){
 	//Input the number of students
